4e587b7c57e9f22c413aaaedbd076884ebbf6ebd
Tayler <t.heidebrecht1@gmail.com>
2026-09-12 11:34:27 -0600

    Now using .isEmpty instead of .equals

--- src/ReadCSV.java
+++ src/ReadCSV.java
@@ -114,14 +114,14 @@ public class ReadCSV {
                         wards = Arrays.copyOf(wards, wards.length + 1);
                     }
 
-                    if (Arrays.asList(wards).contains(item) || (item.equals(""))) {
+                    if (Arrays.asList(wards).contains(item) || (item.isEmpty())) {
 
                     } else {
                         wards[currentPosition++] = item;
                     }
 
                 }
-            } else if (Arrays.asList(wards).contains(row[6]) || (row[6].equals(""))) {
+            } else if (Arrays.asList(wards).contains(row[6]) || (row[6].isEmpty())) {
 
             } else {
                 wards[currentPosition++] = row[6];
